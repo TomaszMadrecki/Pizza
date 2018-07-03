9c4f3a72147c9f3e179445aa52fe0f9553041181
Tomasz Madrecki <tomasz.madrecki@gmail.com>
2018-07-03 18:51:22 +0200

    fix main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Waiter waiter = new Waiter();
         Scanner scanner = new Scanner(System.in);
-        Pizza pizza;
 
         System.out.println("Please insert pizza number");
         System.out.println("1. Margherita");
@@ -18,20 +17,15 @@ public class Main {
         switch (choseNumber) {
             case 1:
                 waiter.setPizzaBuilder(new MargheritaPizzaBuilder());
-                pizza = waiter.getPizza();
                 break;
             case 2:
                 waiter.setPizzaBuilder(new HavaiianPizzaBuilder());
-                pizza = waiter.getPizza();
                 break;
             case 3:
                 waiter.setPizzaBuilder(new FunghiPizzaBuilder());
-                pizza = waiter.getPizza();
                 break;
-            default:
-                pizza = waiter.getPizza();
         }
 
-        System.out.println("This is pizza on: " + pizza);
+        System.out.println("This is pizza on: " + waiter.getPizza());
     }
 }
